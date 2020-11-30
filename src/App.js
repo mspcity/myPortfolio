48c077977bb9d86d0a80d6c978f8a14f25f8dadb
mspcity <codepark2019@gmail.com>
2020-11-30 14:39:11 +0900

    added experience page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
+import Slide from "react-reveal/Slide";
 import { Parallax } from "react-parallax";
 import Container from 'react-bootstrap/Container'
 import BgImage from "./assets/img/parallex/ddd.jpeg"
@@ -10,6 +11,7 @@ import MyCarousal from "./components/my-carousel/my-carousal.component";
 import TitleMessage from "./components/title-message/title-message.component";
 import About from "./pages/about/about.component"
 import Skills from './pages/skills/skills.component'
+import Experience from './pages/experience/experience.component'
 
 import './App.css';
 
@@ -35,8 +37,19 @@ const App = () => {
     {/*  skills section */}
     <div>
         <Container className="container-box rounded">
-          <Fade duration={500}>
+          <Slide bottom duration={500}>
+              <hr />
               <Skills />
+          </Slide>
+        </Container>
+    </div>
+
+    {/* Experience section */}
+    <div>
+        <Container className="container-box rounded">
+          <Fade duration={500}>
+            <hr />
+            <Experience />
           </Fade>
         </Container>
     </div>
--- src/pages/experience/experience.component.jsx
+++ src/pages/experience/experience.component.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
 import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from "react-bootstrap/Container";
 import Tilt from 'react-tilt'
+import Card from "react-bootstrap/Card";
+
+import './experience.style.css'
+
 
 const Experience = () => {
   return (
     <div id='experience'> 
-      
+          <h1 className="pt-3 text-center font-details-b pb-3">EXPERIENCE</h1>
+          <Jumbotron className="jumbo-style">
+            <Container>
+              <Tilt options={{ max: 25 }}>
+              <Card>
+                <Card.Header as="h5" className="d-flex justify-content-center flex-wrap">
+                  {/* <Card.Img variant="top" className="img-resize" src={L_ACCENTURE} alt="Accenture logo" /> */}
+                </Card.Header>
+                <Card.Body className="d-flex justify-content-center flex-column">
+                <div>
+                  <Card.Title className="text-center">Assistant Sales Manager</Card.Title>
+                </div>
+                <div>
+                  <Card.Text className="text-center style">
+                    Duration:
+                    Description:
+                  </Card.Text>
+                </div>
+                </Card.Body>
+              </Card>
+              </Tilt>
+
+            </Container>
+          </Jumbotron>
     </div>
   )
 }
