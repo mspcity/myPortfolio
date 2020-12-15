c6b72cf4053e690d08da1546c415e8720bf7bc83
mspcity <codepark2019@gmail.com>
2020-12-15 00:34:07 -0500

    resume project youtube address changed

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -44,7 +44,7 @@ const About = () => {
                       </a>
                     </div>
                     <div>
-                    <a href="https://drive.google.com/file/d/12IwtIS1AwmCRCI2d0hbbkZ1_gYDRDAl1/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                    <a href="https://drive.google.com/file/d/1PbiupkOCKB2GhA7j72FGSUH-scjil5P8/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                       <Button className="m-2" variant="outline-success">
                         My Resume
                       </Button>
